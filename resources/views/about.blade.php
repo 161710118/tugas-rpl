530c8fdb3f8e34b875205d21e90be38b1c6637dc
andriansyah49 <151610266@smkassalaambandung.sch.id>
2018-01-15 11:01:44 +0700

    laela

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+
+@extends('layouts.1')
+@section('konten')
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tugas</title>
+</head>
+<body>
+<h1> <center>Hatur Nuhun</center> </h1>
+</body>
+</html>
+@endsection
